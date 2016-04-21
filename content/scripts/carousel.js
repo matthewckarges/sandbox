f59c932b214f3a764a0a1e07caedd1a38b168984
Matthew Karges <matthewckarges@gmail.com>
2016-04-20 21:11:59 -0600

    add visibility to javascript object

--- content/scripts/carousel.js
+++ content/scripts/carousel.js
@@ -428,7 +428,7 @@ $.fn.carouselify = function (options) {
                 }
             } else {
 
-                var landscapeMaxImageHeight = .66 * pageHeight;
+                var landscapeMaxImageHeight = .75 * pageHeight;
 
                 if (landscapeMaxImageHeight > this.SlideMaxHeight) {
                     slideHeight = this.SlideMaxHeight;
